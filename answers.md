3ad12f1d6f1cca5fd83ea0bdddfea9adac2de64b
JacksonB-123 <jacksonhardyburch@icloud.com>
2025-04-30 22:49:15 +0000

    lab

--- answers.md
+++ answers.md
@@ -2,7 +2,7 @@
 
 ## Answers
 
-**Name:**_________________________
+**Name:**______Jackson Burch___________________
 **Name:**_________________________
 
 
@@ -12,7 +12,9 @@ Place all written answers from `recitation-08.md` here for easier grading.
 
 - **1b)**
 
+work = O((|V|+|E|)*log|V|)
 
+Span = O(|V|log|V|
 
 - **2b)**
 
--- main.py
+++ main.py
@@ -13,6 +13,33 @@ def shortest_shortest_path(graph, source):
       (shortest path weight, shortest path number of edges). See test case for example.
     """
     ### TODO
+    dist = {}
+    for v in graph:
+        dist[v] = (float('inf'), float('inf'))
+    dist[source] = (0, 0)
+    pq = []
+    heappush(pq, (0, 0, source))
+
+    while pq:
+        total_weight, num_edges, node = heappop(pq)
+
+        if (total_weight, num_edges) > dist[node]:
+            continue
+
+        for neighbor, weight in graph[node]:
+            new_weight = total_weight + weight
+            new_num_edges = num_edges + 1
+
+            if new_weight < dist[neighbor][0] or (
+                new_weight == dist[neighbor][0] and new_num_edges < dist[neighbor][1]):
+                dist[neighbor] = (new_weight, new_num_edges)
+                heappush(pq, (new_weight, new_num_edges, neighbor))
+
+    return dist
+  
+    
+  
+  
     pass
     
 
@@ -25,6 +52,19 @@ def bfs_path(graph, source):
       that vertex in the shortest path tree.
     """
     ###TODO
+    visited = set()
+    queue = deque([source])
+    visited.add(source)
+    queue.append(source)
+    parents = {}
+    while queue:
+        current_node = queue.popleft()
+        for neighbor in graph[current_node]:
+            if neighbor not in visited:
+                visited.add(neighbor)
+                queue.append(neighbor)
+                parents[neighbor] = current_node
+    return parents
     pass
 
 def get_sample_graph():
@@ -44,5 +84,12 @@ def get_path(parents, destination):
       (excluding the destination node itself). See test_get_path for an example.
     """
     ###TODO
+    path = []
+    current_node = destination
+    while current_node in parents:
+        path.append(parents[current_node])
+        current_node = parents[current_node]
+    path.reverse()
+    return ''.join(path)
     pass
 
